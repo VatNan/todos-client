d426ff258495ff6d5d85cb0e47f0ea46c1c80acb
vattanan buranapugdee <vattanan.bu@gmail.com>
2017-11-23 14:27:02 +0700

    create structure of Todo

---
+++ src/containers/Todo/TodoContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { retry } from 'async';
+
+class TodoContainer extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default TodoContainer
--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import gql from 'graphql-tag'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const SERVER_URL = 'http://localhost:3001/graphql'
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: SERVER_URL }),
+  cache: new InMemoryCache()
+})
+
+client.query({ query: gql`{ todos { _id isComplete text } }` }).then(console.log);
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root'))
 registerServiceWorker()
